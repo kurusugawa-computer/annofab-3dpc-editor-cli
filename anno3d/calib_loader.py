180fbc18b0243db670e1678beb459c0ebdb45811
Toshiya MORI <mori@kurusugawa.jp>
2020-05-23 03:33:31 +0900

    calib_loader.pyとテストを追加

---
+++ anno3d/calib_loader.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from typing import Any, Callable, Iterable, List, Optional, TypeVar, cast
+
+from anno3d.model.image import Kitty3DCalib
+
+T = TypeVar("T")
+
+
+def _find(iterable: Iterable[T], pred: Callable[[T], bool]) -> Optional[T]:
+    # pred をキャストしないと以下のエラーで怒られる・・・ 意味がわからない。
+    # error: Argument 1 to "filter" has
+    # incompatible type "Callable[[T], bool]";
+    # expected "Callable[[Optional[T]], Any]"
+
+    return next(filter(cast(Callable[[Optional[T]], Any], pred), iterable), None)
+
+
+def _read_str(lines: List[str]) -> Kitty3DCalib:
+    p2prefix = "P2: "
+    r0prefix = "R0_rect: "
+    velo_cam_prefix = "Tr_velo_to_cam: "
+    p2line = _find(lines, lambda s: s.startswith(p2prefix))
+    r0line = _find(lines, lambda s: s.startswith(r0prefix))
+    velo_cam_line = _find(lines, lambda s: s.startswith(velo_cam_prefix))
+    if p2line is None:
+        raise RuntimeError("p2lineが見つかりませんでした。")
+    if r0line is None:
+        raise RuntimeError("r0lineが見つかりませんでした。")
+    if velo_cam_line is None:
+        raise RuntimeError("velo_cam_lineが見つかりませんでした。")
+
+    def to_values(line: str, prefix: str) -> List[float]:
+        return [float(s) for s in line[len(prefix) :].split(" ")]
+
+    values_p2 = to_values(p2line, p2prefix)
+    values_r0 = to_values(r0line, r0prefix)
+    values_velo_cam = to_values(velo_cam_line, velo_cam_prefix)
+    return Kitty3DCalib(camera_matrix=values_p2, r0_matrix=values_r0, velo_cam_matrix=values_velo_cam)
+
+
+def read_kitty_calib(path: Path) -> Kitty3DCalib:
+    with path.open(mode="r", encoding="utf-8") as file:
+        return _read_str(file.readlines())
---
+++ tests/test_calib_loader.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from anno3d.calib_loader import read_kitty_calib
+
+test_ressources_path = Path(__file__).parent / "resources" / "kitty_calib.txt"
+
+
+def test_read_kitty_calib():
+    calib = read_kitty_calib(test_ressources_path)
+
+    assert len(calib.camera_matrix) == 4 * 3
+    assert calib.camera_matrix[0] == 721.5377
+    assert calib.camera_matrix[9] == 0.0
+    assert calib.camera_matrix[10] == 1.0
+    assert calib.camera_matrix[11] == 2.745884000000e-03
+
+    assert len(calib.r0_matrix) == 3 * 3
+    assert calib.r0_matrix[0] == 9.999239000000e-01
+    assert calib.r0_matrix[8] == 9.999631000000e-01
+
+    assert len(calib.velo_cam_matrix) == 3 * 4
+    assert calib.velo_cam_matrix[0] == 7.533745000000e-03
+    assert calib.velo_cam_matrix[11] == -2.717806000000e-01
