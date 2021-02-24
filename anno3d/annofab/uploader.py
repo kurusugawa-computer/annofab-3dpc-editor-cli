f78c362c6134ce8bff227183e9550b05d6a69bd1
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2021-02-25 01:02:38 +0900

    "--force" を指定すれば、入力データ、補助データを上書きして登録できるようにする

--- anno3d/annofab/uploader.py
+++ anno3d/annofab/uploader.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 from logging import getLogger
 from pathlib import Path
+from typing import Any, Optional
 
+import more_itertools
 import requests
 from annofabapi import AnnofabApi
+from annofabapi.utils import allow_404_error
 
 
 @dataclass(frozen=True)
@@ -19,9 +22,15 @@ class Uploader:
     _client: AnnofabApi
     _project: str
 
-    def __init__(self, client: AnnofabApi, project: str):
+    def __init__(self, client: AnnofabApi, project: str, force: bool = False):
         self._client = client
         self._project = project
+        self._force = force
+
+    @allow_404_error
+    def get_input_data(self, input_data_id: str) -> Optional[Any]:
+        result, _ = self._client.get_input_data(self._project, input_data_id)
+        return result
 
     def upload_tempdata(self, upload_file: Path) -> str:
         client = self._client
@@ -40,6 +49,11 @@ class Uploader:
 
         data_id = input_data_id
         body = {"input_data_name": file.name, "input_data_path": path}
+        if self._force:
+            old_input_data = self.get_input_data(input_data_id)
+            if old_input_data is not None:
+                body["last_updated_datetime"] = old_input_data["updated_datetime"]
+
         input_data, _ = self._client.put_input_data(self._project, data_id, query_params=None, request_body=body)
 
         logger.debug("uploaded input data: %s", input_data)
@@ -47,13 +61,20 @@ class Uploader:
 
     def upload_supplementary(self, input_data_id: str, supplementary_id: str, file: Path) -> str:
         path = self.upload_tempdata(file)
-
         body = {
             "supplementary_data_name": supplementary_id,
             "supplementary_data_path": path,
             "supplementary_data_type": "custom",
             "supplementary_data_number": 0,
         }
+        if self._force:
+            supplementary_list, _ = self._client.get_supplementary_data_list(self._project, input_data_id)
+            old_supplementary = more_itertools.first_true(
+                supplementary_list, pred=lambda e: e["supplementary_data_id"] == supplementary_id
+            )
+            if old_supplementary is not None:
+                body["last_updated_datetime"] = old_supplementary["updated_datetime"]
+
         supplementary, _ = self._client.put_supplementary_data(self._project, input_data_id, supplementary_id, body)
         logger.debug("uploaded supplementary data: %s", supplementary)
         return supplementary_id
