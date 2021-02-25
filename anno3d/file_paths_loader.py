634f317eb44d1a1ae56fe0f289bb8661dcfe633b
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2021-02-25 23:49:32 +0900

    `SceneUploader._default_scene`を公開する

--- anno3d/file_paths_loader.py
+++ anno3d/file_paths_loader.py
@@ -56,7 +56,7 @@ def load_scene_file_paths(scene_path: Path) -> List[FilePaths]:
 
     scene_dir = file.parent
     # TODO: protected memberを呼び出している
-    scene = Scene.decode_path(file) if file.is_file() else SceneUploader._default_scene(scene_path)
+    scene = Scene.decode_path(file) if file.is_file() else SceneUploader.default_scene(scene_path)
 
     def scene_to_paths(frame_id: str) -> FilePaths:
         images = [
