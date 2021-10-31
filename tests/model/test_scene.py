254de59b363bc4cd86af565549e041a7e4532025
maekawatoshiki <konndennsa@gmail.com>
2021-10-31 23:39:45 +0900

    Update test_scene.py

--- tests/model/test_scene.py
+++ tests/model/test_scene.py
@@ -49,6 +49,18 @@ def test_decode_scene():
         "type": "kitti_image",
         "image_dir": "image_3",
         "camera_view_setting": {}
+    },
+    {
+        "type": "kitti_image",
+        "image_dir": "image_4",
+        "file_extension": "jpg"
+    },
+    {
+        "type": "kitti_label",
+        "label_dir": "label_4",
+        "image_dir": "image_4",
+        "calib_dir": "calib",
+        "file_extension": "jpg"
     }
   ],
   // これ以外の項目はoptionとする．有ろうが無かろうが，3DPCE側は気にしない．
@@ -60,8 +72,8 @@ def test_decode_scene():
     assert scene.id_list[0] == "006497"
     assert scene.id_list[1] == "012187"
     assert scene.velodyne.velodyne_dir == "/root/velodyne"
-    assert len(scene.images) == 3
-    assert len(scene.labels) == 1
+    assert len(scene.images) == 4
+    assert len(scene.labels) == 2
 
     image1 = scene.images[0]
     assert image1.image_dir == "/root/image_rf"
@@ -70,18 +82,34 @@ def test_decode_scene():
     assert image1.camera_view_setting.fov == 1.57
     assert image1.camera_view_setting.direction == 0.92
     assert image1.camera_view_setting.position == XYZ(-2.0, -1.0, 0.8)
+    assert image1.file_extension == "png"
 
     image2 = scene.images[1]
     assert image2.image_dir == "/root/image_2"
     assert image2.calib_dir == "/root/calib"
     assert image2.camera_view_setting is None
+    assert image2.file_extension == "png"
 
     image3 = scene.images[2]
     assert image3.image_dir == "/root/image_3"
     assert image3.calib_dir is None
     assert image3.camera_view_setting == CameraViewSettings(None, None, None)
+    assert image3.file_extension == "png"
 
-    label = scene.labels[0]
-    assert label.label_dir == "/root/label_2"
-    assert label.image_dir == "/root/image_2"
-    assert label.calib_dir == "/root/calib"
+    image4 = scene.images[3]
+    assert image4.image_dir == "/root/image_4"
+    assert image4.calib_dir is None
+    assert image4.camera_view_setting is None
+    assert image4.file_extension == "jpg"
+
+    label1 = scene.labels[0]
+    assert label1.label_dir == "/root/label_2"
+    assert label1.image_dir == "/root/image_2"
+    assert label1.calib_dir == "/root/calib"
+    assert label1.file_extension == "png"
+
+    label2 = scene.labels[1]
+    assert label2.label_dir == "/root/label_4"
+    assert label2.image_dir == "/root/image_4"
+    assert label2.calib_dir == "/root/calib"
+    assert label2.file_extension == "jpg"
