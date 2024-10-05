3832580d5ce24520f4be99457b0e687bf37a0427
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2024-10-05 23:28:21 +0900

    format

--- anno3d/app.py
+++ anno3d/app.py
@@ -17,20 +17,12 @@ from anno3d.annofab.uploader import AnnofabStorageUploader, S3Uploader
 from anno3d.file_paths_loader import FilePathsLoader, ScenePathsLoader
 from anno3d.kitti.camera_horizontal_fov_provider import CameraHorizontalFovKind
 from anno3d.kitti.scene_uploader import SceneUploader, SceneUploaderInput, UploadKind
-from anno3d.model.annotation_area import (
-    RectAnnotationArea,
-    SphereAnnotationArea,
-    WholeAnnotationArea,
-)
+from anno3d.model.annotation_area import RectAnnotationArea, SphereAnnotationArea, WholeAnnotationArea
 from anno3d.model.file_paths import FilePaths
 from anno3d.model.frame import PcdFormat
 from anno3d.model.input_files import InputData
 from anno3d.model.scene import Defaults, Scene
-from anno3d.simple_data_uploader import (
-    SupplementaryData,
-    create_kitti_files,
-    upload_async,
-)
+from anno3d.simple_data_uploader import SupplementaryData, create_kitti_files, upload_async
 
 E = TypeVar("E", bound=Enum)
 
@@ -46,11 +38,7 @@ def _decode_enum(enum: Type[E], value: Any) -> E:
 def add_stdout_handler(target: logging.Logger, level: int = logging.INFO):
     handler = logging.StreamHandler()
     handler.setLevel(level)
-    handler.setFormatter(
-        logging.Formatter(
-            "[%(asctime)s] [%(process)d] [%(name)s] [%(levelname)s] %(message)s"
-        )
-    )
+    handler.setFormatter(logging.Formatter("[%(asctime)s] [%(process)d] [%(name)s] [%(levelname)s] %(message)s"))
     target.addHandler(handler)
 
 
@@ -151,10 +139,7 @@ class ProjectCommand:
         """
         if not validate_annofab_credential(annofab_id, annofab_pass, annofab_pat):
             return
-        assert annofab_id is not None and annofab_pass is not None
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
         with client_loader.open_api() as api:
             created_project_id = ProjectApi(api).create_custom_project(
                 title=title,
@@ -198,20 +183,14 @@ class ProjectCommand:
         """
         if not validate_annofab_credential(annofab_id, annofab_pass, annofab_pat):
             return
-        assert annofab_id is not None and annofab_pass is not None
+
         # 数値に変換可能な場合は型がintに変わるので、strに明示的に変換する。
         label_id = str(label_id)
         validate_annofab_credential(annofab_id, annofab_pass, annofab_pat)
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
         with client_loader.open_api() as api:
-            labels = ProjectApi(api).put_cuboid_label(
-                project_id, en_name, label_id, ja_name, color
-            )
-            labels_json = Label.schema().dumps(
-                labels, many=True, ensure_ascii=False, indent=2
-            )
+            labels = ProjectApi(api).put_cuboid_label(project_id, en_name, label_id, ja_name, color)
+            labels_json = Label.schema().dumps(labels, many=True, ensure_ascii=False, indent=2)
             logger.info("Label(=%s) を作成・更新しました", label_id)
             logger.info(labels_json)
 
@@ -258,7 +237,7 @@ class ProjectCommand:
         """
         if not validate_annofab_credential(annofab_id, annofab_pass, annofab_pat):
             return
-        assert annofab_id is not None and annofab_pass is not None
+
         # 数値に変換可能な場合は型がintに変わるので、strに明示的に変換する。
         label_id = str(label_id)
 
@@ -268,13 +247,9 @@ class ProjectCommand:
             )
         safe_segment_type = cast(Literal["SEMANTIC", "INSTANCE"], segment_type)
         if layer < 0:
-            raise RuntimeError(
-                f"layerは、0以上の整数である必要がありますが、{layer} でした"
-            )
+            raise RuntimeError(f"layerは、0以上の整数である必要がありますが、{layer} でした")
 
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
         with client_loader.open_api() as api:
             labels = ProjectApi(api).put_segment_label(
                 project_id,
@@ -286,9 +261,7 @@ class ProjectCommand:
                 ja_name=ja_name,
                 color=color,
             )
-            labels_json = Label.schema().dumps(
-                labels, many=True, ensure_ascii=False, indent=2
-            )
+            labels_json = Label.schema().dumps(labels, many=True, ensure_ascii=False, indent=2)
             logger.info("Label(=%s) を作成・更新しました", label_id)
             logger.info(labels_json)
 
@@ -318,14 +291,9 @@ class ProjectCommand:
         if not validate_annofab_credential(annofab_id, annofab_pass, annofab_pat):
             return
 
-        assert annofab_id is not None and annofab_pass is not None
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
         with client_loader.open_api() as api:
-            new_meta = ProjectApi(api).set_annotation_area(
-                project_id, WholeAnnotationArea()
-            )
+            new_meta = ProjectApi(api).set_annotation_area(project_id, WholeAnnotationArea())
             logger.info("メタデータを更新しました。")
             logger.info(new_meta.to_json(ensure_ascii=False, indent=2))
 
@@ -357,14 +325,9 @@ class ProjectCommand:
         if not validate_annofab_credential(annofab_id, annofab_pass, annofab_pat):
             return
 
-        assert annofab_id is not None and annofab_pass is not None
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
         with client_loader.open_api() as api:
-            new_meta = ProjectApi(api).set_annotation_area(
-                project_id, SphereAnnotationArea(area_radius=str(radius))
-            )
+            new_meta = ProjectApi(api).set_annotation_area(project_id, SphereAnnotationArea(area_radius=str(radius)))
             logger.info("メタデータを更新しました。")
             logger.info(new_meta.to_json(ensure_ascii=False, indent=2))
 
@@ -398,10 +361,7 @@ class ProjectCommand:
         if not validate_annofab_credential(annofab_id, annofab_pass, annofab_pat):
             return
 
-        assert annofab_id is not None and annofab_pass is not None
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
 
         min_x = str(min(x))
         max_x = str(max(x))
@@ -449,10 +409,8 @@ class ProjectCommand:
             return
 
         assert key_name.isalnum()
-        assert annofab_id is not None and annofab_pass is not None
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
 
         with client_loader.open_api() as api:
             new_meta = ProjectApi(api).remove_preset_cuboid_size(project_id, key_name)
@@ -500,10 +458,8 @@ class ProjectCommand:
             return
 
         assert key_name.isalnum()
-        assert annofab_id is not None and annofab_pass is not None
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
 
         with client_loader.open_api() as api:
             new_meta = ProjectApi(api).add_preset_cuboid_size(
@@ -550,7 +506,6 @@ class ProjectCommand:
         if not validate_annofab_credential(annofab_id, annofab_pass, annofab_pat):
             return
 
-        assert annofab_id is not None and annofab_pass is not None
         asyncio.run(
             ProjectCommand._upload_kitti_data_async(
                 project_id,
@@ -588,9 +543,7 @@ class ProjectCommand:
         kitti_dir_path = Path(kitti_dir)
         loader = FilePathsLoader(kitti_dir_path, kitti_dir_path, kitti_dir_path)
         pathss = loader.load(None)[skip : (skip + size)]
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
         sem_opt = asyncio.Semaphore(parallelism) if parallelism is not None else None
 
         async def run_without_sem(
@@ -607,9 +560,7 @@ class ProjectCommand:
                 pcd_format=PcdFormat("xyzi"),
             )
 
-        async def run_with_sem(
-            paths: FilePaths, sem: asyncio.Semaphore
-        ) -> Tuple[str, List[SupplementaryData]]:
+        async def run_with_sem(paths: FilePaths, sem: asyncio.Semaphore) -> Tuple[str, List[SupplementaryData]]:
             async with sem:
                 return await run_without_sem(paths)
 
@@ -690,10 +641,7 @@ class ProjectCommand:
         if not validate_task_id_prefix(task_id_prefix, enum_upload_kind):
             return
 
-        assert annofab_id is not None and annofab_pass is not None
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
         with client_loader.open_api() as api:
             scene_uploader = SceneUploader(
                 api,
@@ -704,9 +652,7 @@ class ProjectCommand:
                 project_id=project_id,
                 input_data_id_prefix=input_data_id_prefix,
                 frame_per_task=frame_per_task,
-                camera_horizontal_fov=_decode_enum(
-                    CameraHorizontalFovKind, camera_horizontal_fov
-                ),
+                camera_horizontal_fov=_decode_enum(CameraHorizontalFovKind, camera_horizontal_fov),
                 sensor_height=sensor_height,
                 task_id_prefix=task_id_prefix,
                 kind=enum_upload_kind,
@@ -760,7 +706,7 @@ class ProjectCommand:
         """
         if not validate_annofab_credential(annofab_id, annofab_pass, annofab_pat):
             return
-        assert annofab_id is not None and annofab_pass is not None
+
         if not validate_aws_credentail():
             return
 
@@ -768,9 +714,7 @@ class ProjectCommand:
         if not validate_task_id_prefix(task_id_prefix, enum_upload_kind):
             return
 
-        client_loader = ClientLoader(
-            annofab_id, annofab_pass, annofab_pat, annofab_endpoint
-        )
+        client_loader = ClientLoader(annofab_id, annofab_pass, annofab_pat, annofab_endpoint)
         with client_loader.open_api() as api:
             uploader = SceneUploader(
                 api,
@@ -781,9 +725,7 @@ class ProjectCommand:
                 project_id=project_id,
                 input_data_id_prefix=input_data_id_prefix,
                 frame_per_task=frame_per_task,
-                camera_horizontal_fov=_decode_enum(
-                    CameraHorizontalFovKind, camera_horizontal_fov
-                ),
+                camera_horizontal_fov=_decode_enum(CameraHorizontalFovKind, camera_horizontal_fov),
                 sensor_height=sensor_height,
                 task_id_prefix=task_id_prefix,
                 kind=enum_upload_kind,
@@ -802,9 +744,7 @@ class LocalCommand:
                 writer.write(input_data.to_json(ensure_ascii=False, sort_keys=True))
                 writer.write("\n")
 
-        logger.info(
-            "%d 件のinput dataを、%sに出力しました", len(inputs), output_dir_path
-        )
+        logger.info("%d 件のinput dataを、%sに出力しました", len(inputs), output_dir_path)
         logger.info("メタデータ: %s", all_files_json.absolute())
 
     @staticmethod
@@ -878,11 +818,7 @@ class LocalCommand:
         if scene_path_.is_dir():
             file = scene_path_ / Defaults.scene_meta_file
 
-        scene = (
-            Scene.decode_path(file)
-            if file.is_file()
-            else Scene.default_scene(scene_path_)
-        )
+        scene = Scene.decode_path(file) if file.is_file() else Scene.default_scene(scene_path_)
         loader = ScenePathsLoader(scene, file.parent)
         pathss = loader.load()
 
@@ -891,9 +827,7 @@ class LocalCommand:
                 input_data_id_prefix,
                 output_dir_path,
                 paths,
-                camera_horizontal_fov=_decode_enum(
-                    CameraHorizontalFovKind, camera_horizontal_fov
-                ),
+                camera_horizontal_fov=_decode_enum(CameraHorizontalFovKind, camera_horizontal_fov),
                 fallback_horizontal_fov=None,
                 sensor_height=sensor_height,
                 pcd_format=PcdFormat(scene.velodyne.format),
